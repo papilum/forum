13e29c3573b79e65ee27dccef2678ca84d05d07c
anatubic <at20201025@student.fon.bg.ac.rs>
2023-07-08 22:38:59 +0200

    kreirane api rute

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('posts', [PostController::class, 'index']);
+    Route::post('posts', [PostController::class, 'store']);
+    Route::put('posts/{id}', [PostController::class, 'update']);
+    Route::delete('posts/{id}', [PostController::class, 'destroy']);
+
+    Route::get('posts/{postId}/comments', [CommentController::class, 'index']);
+    Route::post('posts/{postId}/comments', [CommentController::class, 'store']);
+
+    Route::get('posts/{postId}/likes', [LikeController::class, 'index']);
+    Route::post('posts/{postId}/likes', [LikeController::class, 'store']);
+
+    Route::get('categories', [CategoryController::class, 'index']);
