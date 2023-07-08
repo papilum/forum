5657e1dbcc22021633a0bac83b1ecef7ac2d29ea
papilum <petar.spengler@gmail.com>
2023-07-08 22:15:11 +0200

    kreirane 4 migracije

--- example-app/database/migrations/2023_07_08_200455_create_comments_table.php
+++ example-app/database/migrations/2023_07_08_200455_create_comments_table.php
@@ -15,6 +15,9 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('content');
+            $table->foreignId('user_id');
+            $table->foreignId('post_id');
             $table->timestamps();
         });
     }
