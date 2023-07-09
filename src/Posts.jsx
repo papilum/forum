7a01ff2d5456522e5951dc2bae5046e5411fcecf
DominusDracone <pavleslazarevic@gmail.com>
2023-07-09 23:16:55 +0200

    dodata funkcionalnost pretrage po nazivu posta

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,30 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Posts = ({ posts }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
+
+  const filteredPosts = posts.filter((post) =>
+    post.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="posts">
-      {posts.map((post) => (
-        <div key={post.id} className="post">
-          <h2>{post.title}</h2>
-          <p>{post.content}</p>
-          <p className="author">Author: {post.user.name}</p>
-          <div className="comments-section">
-            <h3>Comments:</h3>
-            {post.comments.length > 0 ? (
-              <ul className="comments-list">
-                {post.comments.map((comment) => (
-                  <li key={comment.id} className="comment">
-                    {comment.content}
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <p>No comments yet.</p>
-            )}
+      <div className="search-bar">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={handleSearchChange}
+          placeholder="Search by title"
+        />
+      </div>
+      {filteredPosts.length > 0 ? (
+        filteredPosts.map((post) => (
+          <div key={post.id} className="post">
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+            <p className="author">Author: {post.user.name}</p>
+            <p className="likes">Likes: {post.likes.length}</p>
+            <div className="comments-section">
+              <h3>Comments:</h3>
+              {post.comments.length > 0 ? (
+                <ul className="comments-list">
+                  {post.comments.map((comment) => (
+                    <li key={comment.id} className="comment">
+                      {comment.content}
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                <p>No comments yet.</p>
+              )}
+            </div>
+           
           </div>
-          <p className="likes">Likes: {post.likes.length}</p>
-        </div>
-      ))}
+        ))
+      ) : (
+        <p>No posts found.</p>
+      )}
     </div>
   );
 };
