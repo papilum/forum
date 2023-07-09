99939e43bdf0cbd80b7029e3f2185e08b9f0074e
anatubic <at20201025@student.fon.bg.ac.rs>
2023-07-09 22:29:41 +0200

    kreirana komponenta navbar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import './App.css';
 import Login from './Login';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Register';
+import Navbar from './Navbar';
 function App() {
   return (
     <Router>
-     
+     <Navbar/>
       <Routes>
         <Route  path="/login" element={<Login></Login>} />
         <Route  path="/register" element={<Register></Register>} />
---
+++ src/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <Link to="/">Forum</Link>
+      </div>
+      <ul className="nav-links">
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
